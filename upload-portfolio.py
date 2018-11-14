d28e96cb0b6e18d0c94527150ae3879376965923
fahd <fahd@fahds-Mac-mini.local>
2018-11-14 14:51:07 +0300

    fic s3locatoin

--- upload-portfolio.py
+++ upload-portfolio.py
@@ -6,42 +6,8 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-<<<<<<< HEAD
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:953723435519:DeployPortfolioTopic')
-=======
-<<<<<<< HEAD
-    sns = boto3.resource('sns')
-    topic = sns.Topic('arn:aws:sns:us-east-1:953723435519:DeployPortfolioTopic')
-
-    try:
-        s3 = boto3.resource('s3') # , config=Config(signature_version='s4v4'))
-
-        portfolio_bucket = s3.Bucket('portfolio.mabyog.com')
-        build_bucket = s3.Bucket('portfoliobuild.mabyog.com')
-
-        portfolio_zip = io.BytesIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-
-
-        with zipfile.ZipFile(portfolio_zip) as myzip:
-             for nm in myzip.namelist():
-                obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm,
-                    ExtraArgs={"ContentType": mimetypes.guess_type(nm)[0]})
-                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-
-        print "Job Done!"
-        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
-    except:
-        topic.publish(Subject="Portfolio Deploy Failed", Message="Portfolio deployed failed!")
-        raise
-
-    return 'Hello from Lambda!'
-=======
-    s3 = boto3.resource('s3') # , config=Config(signature_version='s4v4'))
->>>>>>> 519435bdee7d6ec09de6bbb2821e30a6188ce3bf
 
 
     location = {
@@ -56,7 +22,7 @@ def lambda_handler(event, context):
         if job:
             for artifact in job["data"]["inputArtifacts"]:
                 if artifact["name"] == "MyAppBuild":
-                    location = artifact["location"]["s3location"]
+                    location = artifact["location"]["s3Location"]
 
         print "Building portfolio from " + str(location)
 
@@ -69,7 +35,6 @@ def lambda_handler(event, context):
         build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
 
-<<<<<<< HEAD
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
@@ -87,7 +52,3 @@ def lambda_handler(event, context):
         raise
 
     return 'Hello from Lambda!'
-=======
-ß
->>>>>>> 673fa61200dbf190c50f7882138c42a28efeefd3
->>>>>>> 519435bdee7d6ec09de6bbb2821e30a6188ce3bf
